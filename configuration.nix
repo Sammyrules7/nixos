2861b135f0a95a88c8dc15fe6efb51dfccd3a179
Sammy Scott <sammyrules100@gmaill.com>
2026-02-27 16:26:29 -0700

    Cleaned up a little

--- configuration.nix
+++ configuration.nix
@@ -55,14 +55,8 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sammy = {
