4e6b31f9905cb03da5c1b16ffe2c244e45061e18
Sammy Scott <sammyrules100@gmaill.com>
2026-03-08 11:17:35 -0600

    Various btop fixes for framework 13 amd 7040 edition

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
@@ -9,7 +9,21 @@
 
   networking.hostName = "Sammy_Laptop";
 
-  boot.initrd.kernelModules = [ "tpm_crb" "amdgpu" ];
+  boot.initrd.kernelModules = [
+    "tpm_crb"
+    "amdgpu"
+  ];
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      rocmPackages.clr
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    (btop.override { rocmSupport = true; })
+  ];
 
   boot.initrd.luks.devices."luks-9a6748f1-b660-4f2c-b9fe-40b0dd70c0d7" = {
     device = "/dev/disk/by-uuid/9a6748f1-b660-4f2c-b9fe-40b0dd70c0d7";
