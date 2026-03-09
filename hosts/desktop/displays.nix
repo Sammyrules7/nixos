eca7522f00dbb6a896be028258c68fa8182853c7
Sammy Scott <sammyrules100@gmail.com>
2026-03-08 22:10:21 -0600

    Fixed scale

--- hosts/desktop/displays.nix
+++ hosts/desktop/displays.nix
@@ -1,9 +1,9 @@
 {
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "HDMI-A-1, 1920x1080@60, 0x0, 1"
-      "DP-2, 1920x1080@60, auto-center-left, 1"
-      "DP-3, 1920x1080@60, auto-center-right, 1, transform, 3"
+      "HDMI-A-1, 1920x1080@60, 0x0, 0.8"
+      "DP-2, 1920x1080@60, auto-center-left, 0.8"
+      "DP-3, 1920x1080@60, auto-center-right, 0.8, transform, 3"
     ];
     #workspace = [
     #  "1, monitor:HDMI-A-1"
