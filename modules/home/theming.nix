062ad3b44759483ebac995f28c28c881f25c57cc
Sammy Scott <sammyrules100@gmail.com>
2026-03-09 21:09:09 -0600

    Fixed bad themeing

--- modules/home/theming.nix
+++ modules/home/theming.nix
@@ -7,24 +7,7 @@
     };
   };
 
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-  };
-
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk";
-    style.name = "adwaita-dark";
-  };
-
   home.sessionVariables = {
-    GTK_THEME = "Adwaita-dark";
-    QT_QPA_PLATFORMTHEME = "gtk2";
-
     # Wayland
     NIXOS_OZONE_WL = "1";
     SDL_VIDEODRIVER = "wayland";
