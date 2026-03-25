e02e957cb84274cba89cd5ce5409e467a7da32a1
Sammy Scott <sammyrules100@gmaill.com>
2026-03-24 19:59:12 -0600

    Switched to reputable name servers

--- modules/nixos/networking.nix
+++ modules/nixos/networking.nix
@@ -5,6 +5,19 @@
 
   config = lib.mkIf config.features.networking.enable {
     networking.networkmanager.enable = true;
+    networking.nameservers = [
+      "1.1.1.1"
+      "1.0.0.1"
+      "8.8.8.8"
+      "8.8.4.4"
+      "9.9.9.9"
+      "208.67.222.222"
+      "2606:4700:4700::1111"
+      "2606:4700:4700::1001"
+      "2001:4860:4860::8888"
+      "2001:4860:4860::8844"
+      "2620:fe::fe"
+    ];
     systemd.services.NetworkManager-wait-online.enable = false;
   };
 }
