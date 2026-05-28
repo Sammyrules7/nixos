d556faa859c5485c42fc2add5a940d2a5b10e2ab
Sammy Scott <sammyrules100@gmail.com>
2026-05-28 13:45:55 -0600

    Added monitor config for laptop

--- hosts/laptop/displays.nix
+++ hosts/laptop/displays.nix
@@ -2,6 +2,7 @@
   wayland.windowManager.hyprland.settings = {
     monitor = [
       "eDP-1, 2256x1504@60, auto, 1"
+      "DP-2, 1920x1080@60, auto-center-right, 1"
     ];
     #workspace = [
     #  "1, monitor:HDMI-A-1"
