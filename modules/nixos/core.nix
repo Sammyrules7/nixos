ac878cdcab02ecb3d38a25543aa554ed747568aa
Sammy Scott <sammyrules100@gmail.com>
2026-06-01 23:08:43 -0600

    Added attic cache as substituters

--- modules/nixos/core.nix
+++ modules/nixos/core.nix
@@ -17,12 +17,14 @@
       auto-optimise-store = true;
       substituters = [
         "https://cache.nixos.org/"
+        "https://attic.maio-tech.com/main"
         "https://zen-browser.cachix.org"
         "https://walker.cachix.org"
         "https://walker-git.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "main:arW6XEJpG5vVm3SeAKZ4gohKH6xAKRN2E02iz6vgbXE="
         "zen-browser.cachix.org-1:z/QLGrEkiBYF/7zoHX1Hpuv0B26QrmbVBSy9yDD2tSs="
         "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
